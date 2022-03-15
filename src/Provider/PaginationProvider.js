74d61d0af9b39eab006e92d21cc01b38d658b45e
Fukuyori-Kanta <b1701685@planet.kanazawa-it.ac.jp>
2022-03-16 01:13:49 +0900

    implement_pagination#42

---
+++ src/Provider/PaginationProvider.js
@@ -0,0 +1,25 @@
+﻿import React, {createContext, useState, useContext } from 'react'
+
+const PaginationContext = createContext()
+export const usePagination = () => useContext(PaginationContext)
+
+export default function PaginationProvider({ children }) {
+  //const totalPageCount = NumberUtil.roundByCeil(dataCounts, numberOfDisplaysPerpage)
+  const perPage = 20  // 1ページあたりの表示アイテム数
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // ページクリック時のイベント
+  const handlePaginate = (selectedPage) => {
+    // selectedPage.selectedには、ページ番号 - 1が入る
+    const page = selectedPage.selected + 1
+    console.log(page)
+    setCurrentPage(page)
+    history.pushState({}, '', `?page=${selectedPage.selected}`)
+  }
+
+  return (
+    <PaginationContext.Provider value={{ currentPage, setCurrentPage, handlePaginate, perPage }}>
+      {children}
+    </PaginationContext.Provider>
+  )
+}
