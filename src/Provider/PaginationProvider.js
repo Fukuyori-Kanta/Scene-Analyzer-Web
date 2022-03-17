e7d10e2fac0491e63669e0a9428e3a0669d6fe70
Fukuyori-Kanta <b1701685@planet.kanazawa-it.ac.jp>
2022-03-17 22:00:50 +0900

    resolve_pagination_bug#51

--- src/Provider/PaginationProvider.js
+++ src/Provider/PaginationProvider.js
@@ -4,7 +4,6 @@ const PaginationContext = createContext()
 export const usePagination = () => useContext(PaginationContext)
 
 export default function PaginationProvider({ children }) {
-  //const totalPageCount = NumberUtil.roundByCeil(dataCounts, numberOfDisplaysPerpage)
   const perPage = 20  // 1ページあたりの表示アイテム数
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -15,8 +14,7 @@ export default function PaginationProvider({ children }) {
   // ページクリック時のイベント
   const handlePaginate = (selectedPage) => {
     // selectedPage.selectedには、ページ番号 - 1が入る
-    const page = selectedPage.selected + 1
-    setCurrentPage(page)
+    setCurrentPage(selectedPage.selected + 1)
   }
 
   return (
