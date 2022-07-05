4ec5343f05daacb7cacd65520da209e38e371966
Fukuyori-Kanta <b1701685@planet.kanazawa-it.ac.jp>
2022-07-05 19:24:27 +0900

    show_username#100

---
+++ backend/dbConnection.js
@@ -0,0 +1,27 @@
+﻿// DBへの接続・認証
+const mysql = require('mysql')
+const dbAuthData = require('./config/dbAuthData.json')  // DB認証データ
+const pool = mysql.createPool(dbAuthData)
+
+// DBへの再接続処理
+const handleDisconnect = () => {
+  pool.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack)
+      return
+    }
+    console.log('success')
+  })
+}
+
+// DB接続のエラーハンドリング
+pool.on('error', function (err) {
+  console.log('db error', err)
+  if (err.code === 'PROTOCOL_PoocreatePool_LOST') {
+    handleDisconnect()
+  } else {
+    throw err
+  }
+})
+
+module.exports = pool;
---
+++ src/UserPageModule.js
@@ -0,0 +1,27 @@
+﻿import React from "react"
+import { Fetch } from "./Provider/Fetch"
+
+export default function UserPage() {
+  return (
+    <Fetch
+      uri={`/api/getUserName`}
+      renderSuccess={UserPageContents}
+    />
+  )
+}
+
+function UserPageContents({ data }) {
+
+  const style = {
+    maxWidth: "300px"
+  }
+  console.log(data);
+  /* 各種データの表示 */
+  return (
+    <div className="login-container">
+      <h1>User Page</h1>
+      <div>{data.user}</div>
+    </div>
+
+  )
+}
