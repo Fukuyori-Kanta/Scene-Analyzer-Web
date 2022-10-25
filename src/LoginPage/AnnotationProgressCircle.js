3cb96c521dc3354ad97ba3c02a787843eda105f9
Fukuyori-Kanta <b1701685@planet.kanazawa-it.ac.jp>
2022-10-26 01:07:10 +0900

    implement_annotation_progress_graph_func#117

---
+++ src/LoginPage/AnnotationProgressCircle.js
@@ -0,0 +1,40 @@
+﻿import React, { useEffect, useState } from 'react'
+import Circle from "react-circle"
+
+export default function AnnotationProgressCircle(userId) {
+  const [progress, setProgress] = useState(0) // 進捗率
+  const [workRange, setWorkRange] = useState({ startIdx: 100, workNum: 100 })  // 作業範囲　[作業開始Index, 作業数]
+
+  // レンダリング時に進捗率を設定する関数
+  useEffect(() => {
+    const fetchAnnotationProgress = async () => {
+      // 作業済みレコード数を取得
+      const doneRecordNum = await getAnnotationProgress(userId.userId)
+
+      // 進捗率の設定
+      setProgress(doneRecordNum / workRange.workNum * 100)
+    }
+    fetchAnnotationProgress()
+  }, [])
+
+  // 作業済みレコード数を取得する関数
+  const getAnnotationProgress = async (userId) => {
+    let res = await fetch(`/api/annotationProgress/` + userId)
+    let results = await res.json()
+
+    return results.length
+  }
+
+  return (
+    <div className="progress-circle">
+      <h3 className="circle-title">作業進捗率</h3>
+      <Circle
+        progress={progress}     // 進捗率
+        size={120}              // 円のサイズ
+        bgColor="#D3DEF1"       // 残部分の背景色
+        animationDuration="1s"  // アニメーション時間
+        roundedStroke={true}    // 円の進捗部分に丸みをもたせる
+      />
+    </div>
+  )
+}
--- src/LoginPage/UserPageModule.js
+++ src/LoginPage/UserPageModule.js
@@ -3,6 +3,7 @@ import { useLoggingInUser } from '../Provider/hooks'
 import Breadcrumbs from '../components/Breadcrumbs'
 import SubTitle from '../components/SubTitle'
 import AnnotationHistoryTable from './AnnotationHistoryTable'
+import AnnotationProgressCircle from './AnnotationProgressCircle'
 
 export default function UserPage() {
   return (
@@ -21,15 +22,30 @@ function UserPageContents() {
   return (
     <div className="user-page">
       <Breadcrumbs />
-      <SubTitle heading="ユーザーページ" />
-      <div className="greeting">ようこそ {userInfo.user_name} さん</div>
+      <div className='flex'>
+        <div>
+          <SubTitle heading="ユーザーページ" />
+          <div className="greeting">ようこそ {userInfo.user_name} さん</div>
+        </div>
 
-      <h3 className="table-title">アノテーション作業履歴</h3>
-      {
-        userInfo.user_name != 'guest' &&
-        <AnnotationHistoryTable userId={userInfo.user_id} />
+        { userInfo.user_name != 'guest' &&
+          <>
+            <div className="considerations">
+              <h3>[アノテーション作業中の注意事項]</h3>
+              <h4>※1　1つのCMでのアノテーション作業が終了した際には、Labels ALL Correct にチェックを入れてください</h4>
+              <h4>※2　作業中に不具合が起きた場合は、リロードして該当のシーンから始めてください</h4>
+            </div>
+            <AnnotationProgressCircle userId={userInfo.user_id} />
+          </>
+        }
+      </div>
+
+      { userInfo.user_name != 'guest' &&
+        <>
+          <h3 className="table-title">アノテーション作業履歴</h3>
+          <AnnotationHistoryTable userId={userInfo.user_id} />
+        </>
       }
-
     </div>
   )
 }
